acb6e0edbda04b619a197aa9353fbf15ca848355
eason <abcd8931@yahoo.com.tw>
2022-09-16 11:05:09 +0800

    20220916

--- app.py
+++ app.py
@@ -41,7 +41,7 @@ def msgsend():
     data = json.dumps({"sender": "Rasa", "message": val})
     headers = {'Content-type': 'application/json', 'Accept': 'text/plain'}
     res = requests.post(
-        'https://90db-211-23-17-97.jp.ngrok.io/webhooks/rest/webhook', data=data, headers=headers)
+        'https://dc3f-211-23-17-97.jp.ngrok.io/webhooks/rest/webhook', data=data, headers=headers)
     res = res.json()
     print('------------------------')
     print(res)
